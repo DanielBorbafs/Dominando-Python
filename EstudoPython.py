e4fe18aeb84e418394bb9aaaea9db10b687065e0
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2022-07-12 09:49:20 -0300

    Add files via upload

---
+++ EstudoPython.py
@@ -0,0 +1,66 @@
+# Após a criação dos ambientes virtuais isolados para não haver conflitos podemos começar os estudos#
+
+# PEP 08 Modelagem do Python para tornar a linguagem mais harmônica
+
+
+class Calculadora:
+    pass
+class CalculadoraCientifica:
+    pass
+# Letra maiúscula no inicio da classe - Se for nome composto a primeira letra e a segunda devem ser maiúsculas
+
+
+def soma():
+    pass
+
+   # def soma_dois():
+    #pass
+
+numero = 4
+numero_impar = 5
+
+# Utilizar nomes em minúsculos, separados por underline para funções e variáveis.
+
+if 'a' in 'banana':
+    print('tem')
+# UTILIZAR 4 ESPAÇOS PARA IDENTAÇÃO APÓS OS :
+
+
+class Classe:
+    pass
+
+
+class Outra:
+    pass
+
+
+# SEPARAR FUNÇÕES E DEFINIÇÕES COM DUAS LINHAS EM BRANCO
+# MÉTODOS DENTRO DE UMA CLASSE DEVEM SER SEPARADOS COM UMA ÚNICA LINHA EM BRANCO
+
+
+#IMPORT ERRADO
+import sys, os
+
+#IMPORT CERTO
+ #import sys
+ #import os
+
+# NÃO TEM PROBLEMAS EM USAR:
+
+
+# from types import, Stringtype, ListType
+
+#CASO TENHA MUITOS IMPORTS USAR :
+#from types import
+    #Stringtype,
+    #ListTyper,
+    #SetType,
+    #OutroType,)
+
+
+# IMPORTS Devem ser colocados SEMPRE no topo do arquivo
+
+# DIR - Apresenta todos os atributos e funções/métodos disponíveis para determinado tipo de dado ou variável.
+
+# Help - Apresenta a documentação/como utilizar os atributos/propriedades e funções/Metodos disponíveis.
+
